4c3e608ac115e2f5273fe10c46d57b83c8a093a5
unknown <matisanchez11@gmail.com>
2021-03-15 19:09:23 -0300

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const copyMailDir = str => {
+  const el = document.createElement('textarea');
+  el.value = "devmes11@gmail.com";
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
